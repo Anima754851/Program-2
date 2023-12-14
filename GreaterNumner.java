76d7f2cc96365b5c1bac9f257c3b43c56ba446cc
Anima754851 <137918546+Anima754851@users.noreply.github.com>
2023-12-14 22:11:25 +0530

    Add files via upload

---
+++ GreaterNumner.java
@@ -0,0 +1,21 @@
+package second;
+
+public class GreaterNumner {
+
+	public static void main(String[] args) {
+		int a=10,b=70,c=50;
+		if(a>b)
+		{
+			System.out.println(a+ " is greater");
+		}
+			else if(b>c)
+			{
+				System.out.println(b+ " is greater");
+			}
+				else
+				{
+					System.out.println(c+ " is greater");
+		}
+	}
+
+}
